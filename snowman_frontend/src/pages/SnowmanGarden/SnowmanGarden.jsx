3bd4c19e925e40f968a68e730541efae633f5fdf
lzns960 <lzns960@gmail.com>
2022-12-08 00:44:26 +0900

    feat: snowing

--- snowman_frontend/src/pages/SnowmanGarden/SnowmanGarden.jsx
+++ snowman_frontend/src/pages/SnowmanGarden/SnowmanGarden.jsx
@@ -4,6 +4,7 @@ export default function SnowmanGarden() {
   return (
     <AllContainer>
       <div style={{ color: 'white' }}>메인</div>
+      <img />
       <img
         src={process.env.PUBLIC_URL + '/images/treeHomeSanta.png'}
         alt="treeHomeSanta"
