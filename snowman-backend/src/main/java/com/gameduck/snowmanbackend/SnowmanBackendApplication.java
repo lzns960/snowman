19e27a3041a1572be44cfcf23191271f4b7762ca
Hyeji Lee <zzambbang@gmail.com>
2022-12-09 18:47:50 +0900

    init backend (#9)

---
+++ snowman-backend/src/main/java/com/gameduck/snowmanbackend/SnowmanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gameduck.snowmanbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowmanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnowmanBackendApplication.class, args);
+	}
+
+}
---
+++ snowman-backend/src/test/java/com/gameduck/snowmanbackend/SnowmanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gameduck.snowmanbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowmanBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
