037f385a77f531603ab58a693d045d6e2264c6b8
onezeun <78632052+onezeun@users.noreply.github.com>
2022-12-18 20:43:34 +0900

    feat: 로그인 구현 (#40)

---
+++ snowman_frontend/src/pages/User/Login.jsx
@@ -0,0 +1,199 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
+import AllContainer from '../../components/AllContainer';
+
+import { login } from '../../store/action/auth';
+
+export default function Register() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');;
+  const [loading, setLoading] = useState(false);
+
+  const { isLoggedIn } = useSelector(state => state.auth);
+
+  const { message } = useSelector((state) => state.message);
+  const dispatch = useDispatch();
+
+  const emailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const passwordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const onSubmitHandler = (e) => {
+    console.log('눌렀음');
+    e.preventDefault();
+
+    setLoading(false);
+
+    dispatch(login(email, password ))
+      .then(() => {
+        setLoading(true);
+        alert('로그인 성공!');
+      })
+      .catch(() => {
+        setLoading(false);
+        console.log('실패');
+      });
+
+    navigate('/snowmanGarden');
+  };
+
+  const navigate = useNavigate();
+  const linkSnowmanGarden = () => {
+    navigate('/snowmanGarden');
+  };
+
+  return (
+    <AllContainer>
+      <Main>
+        <LoginTitle>로그인</LoginTitle>
+        <form onSubmit={onSubmitHandler}>
+          <LoginBox>
+            <p>이메일</p>
+            <NameInput onChange={emailChange} />
+            <p>비밀번호</p>
+            <NameInput onChange={passwordChange} />
+          </LoginBox>
+          <BtnBox>
+            <BackBtn onClick={linkSnowmanGarden}>뒤로가기</BackBtn>
+            <LoginBtn type="submit">로그인</LoginBtn>
+          </BtnBox>
+        </form>
+      </Main>
+      <Background>
+        <DarkBg></DarkBg>
+        <TreeHome>
+          <img
+            src={process.env.PUBLIC_URL + '/images/treeHome.png'}
+            alt="tree"
+            style={{
+              width: '100%',
+              objectFit: 'cover',
+              display: 'block',
+              margin: 'auto',
+            }}
+          />
+        </TreeHome>
+        <Snow></Snow>
+      </Background>
+    </AllContainer>
+  );
+}
+
+const Main = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`;
+
+const LoginTitle = styled.div`
+  color: white;
+  font-size: 4rem;
+  margin: 20% 0 10%;
+`;
+
+const LoginBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  padding: 0 5%;
+  & p {
+    font-size: 1.8rem;
+  }
+`;
+
+const NameInput = styled.input`
+  height: 50px;
+  border-radius: 10px;
+  margin-top: 5px;
+  padding: 0 4%;
+  color: black;
+`;
+
+const LoginContent = styled.textarea`
+  width: 100%;
+  height: 40vh;
+  border-radius: 10px;
+  resize: none;
+  white-space: pre-wrap;
+  padding: 4%;
+  box-sizing: border-box;
+  color: black;
+`;
+
+const BtnBox = styled.div`
+  margin: 5% 0;
+  padding: 0 5%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const BackBtn = styled.button`
+  width: 30%;
+  font-size: 1rem;
+  background-color: #999;
+  line-height: 20px;
+
+  padding: 0.6rem 0 0.6rem 0;
+  text-align: center;
+  left: 8%;
+  z-index: 99;
+  border-radius: 5px;
+
+  &:hover {
+    color: white;
+    background-color: #646464;
+    cursor: pointer;
+  }
+`;
+
+const LoginBtn = styled.button`
+  width: 30%;
+  font-size: 1rem;
+  background-color: #ce4545;
+  line-height: 20px;
+
+  padding: 0.6rem 0 0.6rem 0;
+  text-align: center;
+  right: 8%;
+  z-index: 99;
+  border-radius: 5px;
+
+  &:hover {
+    color: white;
+    background-color: rgba(180, 40, 40, 1);
+    cursor: pointer;
+  }
+`;
+
+const Background = styled.div``;
+
+const DarkBg = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  background-color: rgba(0, 0, 0, 0.6);
+  z-index: -1;
+`;
+
+const TreeHome = styled.div`
+  position: absolute;
+  bottom: 40vh;
+  width: 100%;
+  z-index: -2;
+`;
+
+const Snow = styled.div`
+  position: absolute;
+  width: 100%;
+  bottom: 0vh;
+  background-color: white;
+  height: 42vh;
+  z-index: -3;
+`;
