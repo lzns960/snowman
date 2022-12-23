8a2ae8ed616f27d917edad07de34844c7af84f4a
Suji Park <78632299+lzns960@users.noreply.github.com>
2022-12-24 00:07:51 +0900

    fix: base URL 수정 (#81)
    * docs: frontend 폴더명 수정
    
    * fix: baseURL 수정

--- snowman-frontend/src/config.js
+++ snowman-frontend/src/config.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL =
+  'http://https://web-snowman-frontend-20z52flc0kez0t.gksl2.cloudtype.app';
 
 export const API = {
   REGISTER: `${BASE_URL}/api/auth/signup`,
