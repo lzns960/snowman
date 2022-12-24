ba586a3a5399c756b79558d5c213737b4a427d43
Hyeji Lee <zzambbang@gmail.com>
2022-12-24 14:01:47 +0900

    fix: 프록시 미들웨어 설정 (#96)
    * feat: gradle-wrapper.jar 추가
    
    * fix: 백엔드 cors 설정
    
    * fix: front cors 설정
    
    * fix: front cors설정
    
    * fix: front cors설정
    
    * fix: axios.defaults.headers['Access-Control-Allow-Origin'] = '*';
    
    * fix: 프록시 미들웨어 설정

---
+++ snowman-frontend/src/util/setProxy.js
@@ -0,0 +1,10 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware("/api", {
+      target: "https://port-0-snowman-20z52flc1ehpg7.gksl2.cloudtype.app",
+      changeOrigin: true,
+    })
+  );
+};
