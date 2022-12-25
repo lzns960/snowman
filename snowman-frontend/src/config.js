01d0d5440590dcd31d58488c282d6d5db88ed094
onezeun <78632052+onezeun@users.noreply.github.com>
2022-12-25 11:44:15 +0900

    fix: 프록시 서버 url 추가 (#100)

--- snowman-frontend/src/config.js
+++ snowman-frontend/src/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://web-snowman-frontend-20z52flc0kez0t.gksl2.cloudtype.app';
+const BASE_URL = 'http://cors-anywhere.herokuapp.com/https://web-snowman-frontend-20z52flc0kez0t.gksl2.cloudtype.app';
 export const API = {
   REGISTER: `${BASE_URL}/api/auth/signup`,
   LOGIN: `${BASE_URL}/api/auth/login`,
