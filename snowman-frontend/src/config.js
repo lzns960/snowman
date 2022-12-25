aecaeada5c8ed2b0d9ab8306eca3e33ec34abddb
onezeun <78632052+onezeun@users.noreply.github.com>
2022-12-25 14:33:12 +0900

    fix: 프록시 url 변경 (#107)
    * fix: 프록시 서버 url 추가
    
    * fix: 배포관련오류수정
    
    * fix: 배포관련오류수정
    
    * fix: 배포관련오류수정
    
    * fix: 배포관련오류수정
    
    * feat: env 설정
    
    * fix: 배포관련오류수정
    
    * fix:  프록시 url 변경

--- snowman-frontend/src/config.js
+++ snowman-frontend/src/config.js
@@ -1,5 +1,5 @@
-// const BASE_URL = 'https://web-snowman-20z52flc2u4w7q.gksl2.cloudtype.app/';
-const BASE_URL = process.env.REACT_APP_SNOWMAN_API_URL;
+const BASE_URL = 'https://proxy.cors.sh/https://web-snowman-20z52flc2u4w7q.gksl2.cloudtype.app/';
+// const BASE_URL = process.env.REACT_APP_SNOWMAN_API_URL;
 
 export const API = {
   REGISTER: `${BASE_URL}/api/auth/signup`,
