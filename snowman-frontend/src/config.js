90a9c5f4b6bdc0ead1c0b481ddb629b40c4efcea
onezeun <78632052+onezeun@users.noreply.github.com>
2022-12-25 13:11:43 +0900

    fix: 배포관련오류수정 (#101)
    * fix: 프록시 서버 url 추가
    
    * fix: 배포관련오류수정

--- snowman-frontend/src/config.js
+++ snowman-frontend/src/config.js
@@ -1,4 +1,5 @@
 const BASE_URL = 'http://cors-anywhere.herokuapp.com/https://web-snowman-frontend-20z52flc0kez0t.gksl2.cloudtype.app';
+// const BASE_URL = process.env.REACT_APP_SNOWMAN_API_URL;
 export const API = {
   REGISTER: `${BASE_URL}/api/auth/signup`,
   LOGIN: `${BASE_URL}/api/auth/login`,
--- snowman-frontend/src/util/setProxy.js
+++ snowman-frontend/src/util/setProxy.js
@@ -1,10 +1,10 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+// const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function (app) {
-  app.use(
-    createProxyMiddleware("", {
-      target: "https://port-0-snowman-20z52flc1ehpg7.gksl2.cloudtype.app",
-      changeOrigin: true,
-    })
-  );
-};
+// module.exports = function (app) {
+//   app.use(
+//     createProxyMiddleware("", {
+//       target: "https://port-0-snowman-20z52flc1ehpg7.gksl2.cloudtype.app",
+//       changeOrigin: true,
+//     })
+//   );
+// };
