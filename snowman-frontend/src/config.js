ca44cbd9fb3bb4de9d872770655ec582de54e695
onezeun <78632052+onezeun@users.noreply.github.com>
2022-12-25 14:06:26 +0900

    feat: env 설정 (#105)
    * fix: 프록시 서버 url 추가
    
    * fix: 배포관련오류수정
    
    * fix: 배포관련오류수정
    
    * fix: 배포관련오류수정
    
    * fix: 배포관련오류수정
    
    * feat: env 설정

--- snowman-frontend/src/config.js
+++ snowman-frontend/src/config.js
@@ -1,6 +1,6 @@
-const BASE_URL = 'https://cors.bridged.cc/https://web-snowman-20z52flc2u4w7q.gksl2.cloudtype.app/';
+// const BASE_URL = 'https://web-snowman-20z52flc2u4w7q.gksl2.cloudtype.app/';
+const BASE_URL = process.env.REACT_APP_SNOWMAN_API_URL;
 
-// const BASE_URL = process.env.REACT_APP_SNOWMAN_API_URL;
 export const API = {
   REGISTER: `${BASE_URL}/api/auth/signup`,
   LOGIN: `${BASE_URL}/api/auth/login`,
