3423b8a20d2273ba8703aa6039ffbb153e402ab5
Suji Park <78632299+lzns960@users.noreply.github.com>
2022-12-29 01:04:46 +0900

    fix: proxy 설정 되돌리기 (#120)
    * docs: repo 통합
    
    * fix: allowedOrigins 도메인 변경
    
    * fix: config BASE_URL 수정
    
    * fix: Proxy 파일명 수정
    
    * fix: proxy 수정
    
    * fix: proxy 설정 되돌리기

--- snowman-frontend/src/setupProxy.js
+++ snowman-frontend/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware("/api", {
+    createProxyMiddleware(" ", {
       target: "https://port-0-backend-20z52flc2u4w7q.gksl2.cloudtype.app/",
       changeOrigin: true,
     })
