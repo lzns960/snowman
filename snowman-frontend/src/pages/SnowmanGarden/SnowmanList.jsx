47562e70bdc05d72747d9aa141d57f8b58f7f498
Suji Park <78632299+lzns960@users.noreply.github.com>
2022-12-31 02:36:35 +0900

    fix: update snowmanList

--- snowman-frontend/src/pages/SnowmanGarden/SnowmanList.jsx
+++ snowman-frontend/src/pages/SnowmanGarden/SnowmanList.jsx
@@ -24,7 +24,7 @@ export default function SnowmanList({ data, gardenEmail }) {
           data
             .slice(pagePost * (page - 1), pagePost * (page - 1) + pagePost)
             .map((a, i) => {
-              snowmanList.push(a.snowman_id);
+              snowmanList.push(a.snowmanId);
               console.log(snowmanList);
               return (
                 <div key={a.snowmanId}>
