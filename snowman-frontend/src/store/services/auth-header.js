4f59248e374f010e15eb89189c50104c48a16f96
Suji Park <78632299+lzns960@users.noreply.github.com>
2022-12-30 15:42:11 +0900

    fix: axios.create Headers 설정

--- snowman-frontend/src/store/services/auth-header.js
+++ snowman-frontend/src/store/services/auth-header.js
@@ -1,8 +1,12 @@
+import axios from 'axios';
+
 export default function authHeader() {
   const user = JSON.parse(localStorage.getItem('user'));
 
   if (user && user.accessToken) {
-    return { Authorization: 'Bearer ' + user.accessToken };
+    return axios.create({
+      headers: { Authorization: 'Bearer ' + user.accessToken }
+    })
   } else {
     return {};
   }
