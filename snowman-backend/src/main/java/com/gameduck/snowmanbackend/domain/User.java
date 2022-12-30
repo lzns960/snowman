6abf3d04799a9f33ba99d7eeec6d2f75fa44bb3c
Hyeji Lee <zzambbang@gmail.com>
2022-12-30 21:34:30 +0900

    feat: import (#124)

--- snowman-backend/src/main/java/com/gameduck/snowmanbackend/domain/User.java
+++ snowman-backend/src/main/java/com/gameduck/snowmanbackend/domain/User.java
@@ -10,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
@@ -25,7 +26,7 @@ import lombok.Setter;
 public class User {
 
 	@Id
- @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "user_id")
 	private Long id;
 
